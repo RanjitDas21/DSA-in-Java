9666aac8da48d8554681d28f47bd65097feaf554
Ranjit Das <ranjitdasajodhyapur38@gmail.com>
2025-03-04 00:20:03 +0530

    Add dead maze path-2D

---
+++ Backtracking/deadMazePath2D.java
@@ -0,0 +1,29 @@
+public class deadMazePath2D {
+    public static void main(String[] args) {
+        int rows = 4;
+        int cols = 6;
+        int[][] maze = {{1,0,1,1,1,1},
+                {1,1,1,1,0,1},
+                {0,1,1,1,1,1},
+                {0,0,1,0,1,1}};
+        printMazePath(0,0,rows-1,cols-1,"",maze);
+    }
+
+    private static void printMazePath(int sr, int sc, int er, int ec, String str, int[][] maze) {
+        if(sr>er || sc>ec){
+            return;
+        }
+
+        if(sr == er && sc == ec){
+            System.out.println(str);
+            return;
+        }if(maze[sr][sc] == 0){
+            return;
+        }
+
+        //go right
+        printMazePath(sr,sc+1,er,ec,str+"R",maze);
+        //go down
+        printMazePath(sr+1,sc,er,ec,str+"D",maze);
+    }
+}
