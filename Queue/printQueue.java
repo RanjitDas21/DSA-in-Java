7e4370eb3ae120b6a71f94356670ec1baffaeac9
Ranjit Das <ranjitdasajodhyapur38@gmail.com>
2025-03-03 23:56:32 +0530

    Add print Queue

---
+++ Queue/printQueue.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class printQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(3);
+        q.add(1);
+        q.add(8);
+        q.add(4);
+        q.add(7);
+        Queue<Integer> helper = new LinkedList<>();
+        while(q.size() > 0) {
+            System.out.print(q.peek()+" ");
+            helper.add(q.poll());
+        }
+        while(helper.size() > 0) {
+            q.add(helper.poll());
+        }
+    }
+}
